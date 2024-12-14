1a0360cf251414c3357c6915db8b37a7ec3489f4
laurarj24 <lromerovet@gmail.com>
2024-12-14 14:00:48 +0100

    cambio diccionario

--- juego_preguntas.py
+++ juego_preguntas.py
@@ -6,16 +6,6 @@
 
 preguntas = {
     "¿Cuál es la capital de Canadá?": "Ottawa",
-    "¿Qué planeta es conocido como el Planeta Rojo?": "Marte",
-    "¿En qué año comenzó la Segunda Guerra Mundial?": "1939",
-    "¿Cuál es el símbolo químico del oro?": "Au",
-    "¿Quién pintó La Última Cena?": "Leonardo da Vinci",
-    "¿Qué océano es el más grande del mundo?": "Océano Pacífico",
-    "¿Quién escribió *Don Quijote de la Mancha*?": "Miguel de Cervantes",
-    "¿Qué país tiene forma de bota en el mapa?": "Italia",
-    "¿Cómo se llama la molécula que contiene la información genética?": "ADN",
-    "¿Cuántos jugadores hay en un equipo de baloncesto en la cancha?": "Cinco",
-    "¿Cuál es el idioma oficial de Brasil?": "Portugués",
     "¿Qué montaña es la más alta del mundo?": "Monte Everest",
     "¿Qué número romano representa el 50?": "L",
     "¿Qué elemento tiene el número atómico 1?": "Hidrógeno",
