93290d1625dc9b45e6dd7641f86da57afcacd1d1
laurarj24 <lromerovet@gmail.com>
2024-12-14 13:47:44 +0100

    lib

--- juego_preguntas.py
+++ juego_preguntas.py
@@ -1,5 +1,7 @@
 # TAREA 1 ==> Importar librerias
-
+import re
+import random
+import tkinter as tk
 # TAREA 2 ==> Desarrollar funcion que ejecute el juego
     
 # TAREA 3 ==> Hacer los diccionarios de preguntas y respuestas
