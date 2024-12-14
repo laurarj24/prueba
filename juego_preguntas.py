d493ff84276d310b331be308aa8bc49330773f96
laurarj24 <lromerovet@gmail.com>
2024-12-14 14:33:49 +0100

    cambio diccionario

--- juego_preguntas.py
+++ juego_preguntas.py
@@ -1,10 +1,3 @@
-
-# TAREA 1 ==> Importar librerias
-
-
-# TAREA 2 ==> Desarrollar funcion que ejecute el juego
-
-
 # TAREA 3 ==> Hacer los diccionarios de preguntas y respuestas
 
 preguntas_faciles = {
@@ -16,3 +9,12 @@ preguntas_faciles = {
     "¿Qué océano es el más grande del mundo?": "Océano Pacífico",
     "¿Quién escribió *Don Quijote de la Mancha*?": "Miguel de Cervantes",
 }
+
+
+# TAREA 1 ==> Importar librerias
+
+
+# TAREA 2 ==> Desarrollar funcion que ejecute el juego
+
+
+
