d6ea2fbe40a98bf437df2c58b745b199e81eb5bc
laurarj24 <lromerovet@gmail.com>
2025-06-14 12:17:49 +0200

    nombre y edad

--- hola.py
+++ hola.py
@@ -1,21 +1,3 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "nombre = \"Eva\"\n",
-    "edad = 37\n"
-   ]
-  }
- ],
- "metadata": {
-  "language_info": {
-   "name": "python"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+nombre = 'Elena'
+edad = 29
+
